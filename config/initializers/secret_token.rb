ffcd0c10185c5bce8c0025894db2049763ed1b63
Christopher O'Donnell <codonnell822@gmail.com>
2012-06-17 13:10:50 -0400

    generated a basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codly::Application.config.secret_token = '1896d0812a399ebe8eb80bbba35f9c4d82b5577465fb2aaa094edf5e9d54d14d0df3ee421894fb176e1e32a8da816116477ac6ed568b716cf6dc01b0cffebd1b'

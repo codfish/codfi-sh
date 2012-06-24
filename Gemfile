9f7015f46df21df0169bf76162a39b9ed2346ac2
Christopher O'Donnell <codonnell822@gmail.com>
2012-06-24 11:37:27 -0400

    user model and forms, bootstrap

--- Gemfile
+++ Gemfile
@@ -6,7 +6,9 @@ gem 'rails', '3.2.5'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'pg'
-
+gem 'devise'
+gem 'jquery-rails'
+gem 'twitter-bootstrap-rails'
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -19,8 +21,6 @@ group :assets do
 
   gem 'uglifier', '>= 1.0.3'
 end
-
-gem 'jquery-rails'
 
 group :development, :test do
   gem 'rspec-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Codly::Application.routes.draw do
+  devise_for :users
+  
+  devise_scope :user do
+	  get "sign_in", :to => "devise/sessions#new"
+	end
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ Codly::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 

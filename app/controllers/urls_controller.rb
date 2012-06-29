eabf5d54345fdc9551d57ab0c6599710775c557e
Christopher O'Donnell <codonnell822@gmail.com>
2012-06-29 17:29:43 -0400

    fixed code spacing, url scopes, nested route queries

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,63 +1,59 @@
 class UrlsController < ApplicationController
 
 	before_filter :authenticate_user!, :only => [:edit, :update, :destroy]
-  before_filter :get_url, :only => [ :edit, :show, :update, :destroy ]
-  
-	#default_scope order("redirect_count DESC"), :default => true 
-
+	before_filter :get_url, :only => [ :edit, :show, :update, :destroy ]
+	
   def index
-  	if user_signed_in?
-	  	@urls = current_user.urls.all
-  	else 
-	    @urls = Url.order("created_at DESC")
-	  end
-	  
-	  if params[:popular]
-	  	@popular = true
-	  end
-	  
-	  @url = Url.new	  
+    if params[:user_id]
+      @urls = current_user.urls.all
+    elsif params[:popular]
+      @urls = Url.all
+      @popular = true
+    else
+      @urls = Url.unscoped.order("created_at DESC")
+    end
+    
+    @url = Url.new	  
   end
-
+  
   def show
   end
-
+  
   def new
     @url = Url.new
   end
-
+  
   def edit
   end
   
   def create
-	  @url = Url.find_by_full_url(params[:url][:full_url])
-	  if user_signed_in? 
-	  	@user_url_exists = current_user.urls.find_by_full_url(params[:url][:full_url])
-		end
-	  
-	  if (@url)
-	  	if @user_url_exists
-	  		redirect_to urls_url, notice: 'You already created that short url.' and return
-	  	else
-	  		@url.users << current_user if (user_signed_in?)
-	  	end
-	  else
-			@url = Url.new(params[:url])
-		  @url.users << current_user if (user_signed_in?)
-	  end
-	  
-	  if (@url.valid? && @url.short_url)
-	  	@url.dont_shorten_me!
-	  end
-	  
-	  if @url.save
+    @url = Url.find_by_full_url(params[:url][:full_url])
+    if user_signed_in? 
+      @user_url_exists = current_user.urls.find_by_full_url(params[:url][:full_url])
+    end
+  
+    if (@url)
+      if @user_url_exists
+        redirect_to urls_url, notice: 'You already created that short url.' and return
+      else
+        @url.users << current_user if (user_signed_in?)
+      end
+    else
+      @url = Url.new(params[:url])
+      @url.users << current_user if (user_signed_in?)
+    end
+  
+    if (@url.valid? && @url.short_url)
+      @url.dont_shorten_me!
+    end
+    
+    if @url.save
       redirect_to root_path
     else
       render action: "new"
-    end
-		
+    end  
   end
-
+  
   def update
     if @url.update_attributes(params[:url])
       redirect_to @url, notice: 'Url was successfully updated.'
@@ -65,16 +61,16 @@ class UrlsController < ApplicationController
       render action: "edit"
     end
   end
-
+  
   def destroy
     @url = Url.find(params[:id])
     @url.destroy		
-		redirect_to urls_url
+    redirect_to urls_url
   end
   
   def redirect
     if @url = Url.find_by_short_url(params[:short_url])
-    	@url.increment_redirect_count!
+      @url.increment_redirect_count!
       redirect_to @url.full_url
     else
       redirect_to urls_url
@@ -84,6 +80,6 @@ class UrlsController < ApplicationController
   private 
   
   def get_url
-	  @url = Url.find(params[:id])
-	end
+    @url = Url.find(params[:id])
+  end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,20 +1,20 @@
 class Url < ActiveRecord::Base
   attr_accessible :full_url, :redirect_count, :short_url
   
-  before_validation :initialize_shorten_me
-  validates 				:full_url, :presence => true, :format => { :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix, :message => "Not a valid URL" }
-  validate 					:not_linked_to_codly?
-  before_save 	:set_short_url
+  before_validation  :initialize_shorten_me
+  validates          :full_url, :presence => true, :format => { :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix, :message => "Not a valid URL" }
+  validate           :not_linked_to_codly?
+  before_save        :set_short_url
   
   has_many :user_urls
   has_many :users, :through => :user_urls
   
-#   default_scope o
+  default_scope order("redirect_count DESC")
 
   def initialize_shorten_me
     @shorten_me = true
   end
-
+  
   def increment_redirect_count!
     update_attribute(:redirect_count, redirect_count + 1)
   end
@@ -26,30 +26,27 @@ class Url < ActiveRecord::Base
   private
   
   def not_linked_to_codly?
-  	errors.add(:full_url, "can't link to cod.ly.") if (self.full_url.include?('cod.ly') || self.full_url.include?('http://codly') || self.full_url.include?('localhost'))
+    errors.add(:full_url, "can't link to cod.ly.") if (self.full_url.include?('cod.ly') || self.full_url.include?('http://codly') || self.full_url.include?('localhost'))
   end
   
   def set_short_url
-		self.short_url = Url.create_short_url_path if @shorten_me
+    self.short_url = Url.create_short_url_path if @shorten_me
   end
   
   # characters to use when generating your short url path (0..9,a..z,A-Z)
-	# 56,800,235,584 possibilities with the 62 based character set
-	# 68,719,476,736 with 64 based (can add _ and -, maybe some others)
-	CHARSET = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-	BASE = 62
-	SHORT_URL_LENGTH = 6
-	
+  # 56,800,235,584 possibilities with the 62 based character set
+  # 68,719,476,736 with 64 based (can add _ and -, maybe some others)
+  CHARSET = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  BASE = 62
+  SHORT_URL_LENGTH = 6
+  
   def self.create_short_url_path
-		@short_url = ""
-		for i in 1..SHORT_URL_LENGTH
-			@short_url += CHARSET[rand(BASE)]
-		end
-		
-		self.create_short_url_path if Url.find_by_short_url(@short_url)
-	  
-		return @short_url
+    @short_url = ""
+    for i in 1..SHORT_URL_LENGTH
+      @short_url += CHARSET[rand(BASE)]
+    end
+    self.create_short_url_path if Url.find_by_short_url(@short_url)
+    
+    return @short_url
   end
-
-
 end
